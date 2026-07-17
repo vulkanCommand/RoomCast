f8b2ef8c9d14edc6c389de754778f36eacaea89d
vulkanCommand <gdkalyan2109@gmail.com>
2026-07-17 10:46:10 -0400

    chore: deploy latest RoomCast updates (2026-07-17 10:46:10)

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "react-hooks/purity": "off",
+      "react-hooks/set-state-in-effect": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
     },
